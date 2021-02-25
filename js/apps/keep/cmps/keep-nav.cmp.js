a9a9a08a5afcff50c68ae09398c16928824c8075
ReamLasry <ream794613@gmail.com>
2021-02-25 19:08:04 +0200

    input-component update

--- js/apps/keep/cmps/keep-nav.cmp.js
+++ js/apps/keep/cmps/keep-nav.cmp.js
@@ -1,11 +1,14 @@
 import { eventBus } from '../../../services/event-bus.service.js';
+import keepInput from '../cmps/keep-input.cmp.js';
+
 export default {
     template: `
     <section>
             <nav class="flex email-nav space-between align-center" >
                 <div class="keep-logo">Keep</div>    
                 <form>
-                    <input v-model="newNoteTxt" @input="txt" type="text" placeholder="New note..." />
+                    <keep-input />
+                    <!-- <input v-model="newNoteTxt" @input="txt" type="text" placeholder="New note..." /> -->
                     <select v-model="filterBy" @change="setFilter" name="keep-filter-selector">
                         <option value="all">All</option>
                         <option value="inbox">Video</option>
@@ -19,7 +22,7 @@ export default {
     data() {
         return {
             filterBy: "all",
-            newNoteTxt:null
+            newNoteTxt: null
         }
     },
     methods: {
@@ -27,8 +30,8 @@ export default {
             var filter = this.filterBy;
             eventBus.$emit('filtered', filter)
         }
+    },
+    components: {
+        keepInput
     }
 }
-
-
-
--- js/apps/keep/pages/keep-page.cmp.js
+++ js/apps/keep/pages/keep-page.cmp.js
@@ -1,15 +1,16 @@
 import keepNav from '../cmps/keep-nav.cmp.js';
 import keepDev from '../cmps/keep-dev.cmp.js';
-import keepInput from '../cmps/keep-input.cmp.js';
 import keepList from '../cmps/keep-list.cmp.js';
 import { keepService } from '../services/keep.service.js';
+import keepCompose from '../cmps/keep-compose.cmp.js';
+
 export default {
 
     template: `
     <section>
         <keep-nav />
         <keep-dev />
-        <keep-input />
+        <keep-compose />
         <keep-list />
     </section>
     `,
@@ -33,7 +34,7 @@ export default {
     components: {
         keepNav,
         keepDev,
-        keepInput,
-        keepList
+        keepList,
+        keepCompose
     }
 }
