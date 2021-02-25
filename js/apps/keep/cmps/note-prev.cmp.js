b936ad8cfe0abcb03ba52ee80702f7349a399e8d
Noa Reisman <noareisman10@gmail.com>
2021-02-25 20:56:18 +0200

    submission2

--- js/apps/keep/cmps/note-prev.cmp.js
+++ js/apps/keep/cmps/note-prev.cmp.js
@@ -1,28 +1,48 @@
+import noteImg  from './note-img.cmp.js';
+import noteTxt  from './note-txt.cmp.js';
+import noteVideo  from './note-video.cmp.js';
+import noteTodo  from './note-todo.cmp.js';
 import { keepService } from '../services/keep.service.js';
 export default {
+    props: ['note'],
     template: `
         <section> 
-            <compotent  :is="noteType"
-                        :info=""
-                        @pin="pinNote"
-                        @edit="editNote">
                 <div class="note-container">
                     <button @click="pin">Pin</button>        
                     <button @click="Edit">Edit</button>
                     <pre>{{this.txt}}</pre>
-                    <img :src="file"/>
+                    <compotent :is="type" :info="info"/>
                 </div>        
-            <component/>
         </section>
-    `,
+        `,
     data() {
         return {
-            comp:'null'
+
+            id:null,
+            type:null,
+            pin:null,
+            info:{},
+            style:{}
         }
     },
-    crearte(){
-        keepService.query()
+    methods:{
+        pin(){            
+        }
+    },
+    components:{
+        noteTodo,
+        noteTxt,
+        noteVideo,
+        noteImg
     }
+    created(){
+        this.type=this.note.type,
+        this.pin=this.note.isPined,
+        this.info=this.note.info,
+        this.style=this.note.style,
+        this.id=this.note.id
+    }
+
 }
 
-<component :is="cmp.type"  :info="cmp.info" @setVal="setAns($event, idx)"></component>
+    // < component : is = "cmp.type"  : info = "cmp.info" @setVal="setAns($event, idx)" ></component >
