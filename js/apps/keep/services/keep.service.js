fa25399a5d7d01cfdb7805a5f7a2cc2d86952ff5
ReamLasry <ream794613@gmail.com>
2021-02-25 16:16:00 +0200

    service update

--- js/apps/keep/services/keep.service.js
+++ js/apps/keep/services/keep.service.js
@@ -1,3 +1,28 @@
 import { asyncStorageService } from '../../../services/async-storage.service.js';
 
 const KEEPS_KEY = 'keeps';
+const gTempNotes = [{
+    type: "NoteTxt",
+    isPinned: true,
+    info: {
+        txt: "Fullstack Me Baby!"
+    }
+}, {
+    type: "NoteImg",
+    info: {
+        url: "http://some-img/me",
+        title: "Me playing Mi"
+    },
+    style: {
+        backgroundColor: "#00d"
+    }
+}, {
+    type: "NoteTodos",
+    info: {
+        label: "How was it:",
+        todos: [
+            { txt: "Do that", doneAt: null },
+            { txt: "Do this", doneAt: 187111111 }
+        ]
+    }
+}];
