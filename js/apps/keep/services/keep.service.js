6251e889769d4b8642dbdf49098372f561f2e728
ReamLasry <ream794613@gmail.com>
2021-02-27 00:29:30 +0200

    update

--- js/apps/keep/services/keep.service.js
+++ js/apps/keep/services/keep.service.js
@@ -3,63 +3,63 @@ import { asyncStorageService } from '../../../services/async-storage.service.js'
 const KEEPS_KEY = 'keeps';
 
 const gTempKeeps = [{
-    id: 'TempTry1',
-    type: 'noteTxt',
-    isPinned: false,
-    info: {
-        title: '',
-        txt: 'Fullstack Me Baby!'
+        id: 'TempTry1',
+        type: 'noteTxt',
+        isPinned: false,
+        info: {
+            title: '',
+            txt: 'Fullstack Me Baby!'
+        },
+        style: {
+            backgroundColor: 'moccasin',
+            color: 'red'
+        }
     },
-    style: {
-        backgroundColor: 'moccasin',
-        color: 'red'
-    }
-},
-{
-    id: 'TempTry2',
-    type: 'noteImg',
-    isPinned: false,
-    info: {
-        url: 'https://images.unsplash.com/photo-1469598614039-ccfeb0a21111?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2134&q=80',
-        title: 'Me playing Mi',
-        txt: ''
+    {
+        id: 'TempTry2',
+        type: 'noteImg',
+        isPinned: false,
+        info: {
+            url: 'https://images.unsplash.com/photo-1469598614039-ccfeb0a21111?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2134&q=80',
+            title: 'Me playing Mi',
+            txt: ''
+        },
+        style: {
+            backgroundColor: '#f8d2e4',
+            color: 'black'
+        }
     },
-    style: {
-        backgroundColor: '#f8d2e4',
-        color: 'black'
-    }
-},
-{
-    id: 'TempTry3',
-    type: 'noteTodos',
-    isPinned: false,
-    info: {
-        title: '',
-        label: 'How was it:',
-        todos: [
-            { txt: 'Do that', doneAt: null },
-            { txt: 'Do this', doneAt: 187111111 }
-        ]
+    {
+        id: 'TempTry3',
+        type: 'noteTodos',
+        isPinned: false,
+        info: {
+            title: '',
+            label: 'How was it:',
+            todos: [
+                { txt: 'Do that', doneAt: null },
+                { txt: 'Do this', doneAt: 187111111 }
+            ]
+        },
+        style: {
+            backgroundColor: 'white',
+            color: 'black'
+        }
     },
-    style: {
-        backgroundColor: 'white',
-        color: 'black'
+    {
+        id: 'TempTry4',
+        type: 'noteVideo',
+        isPinned: false,
+        info: {
+            url: 'https://i.ytimg.com/an_webp/CduA0TULnow/mqdefault_6s.webp?du=3000&sqp=COzt44EG&rs=AOn4CLBxY6Ssg3C6cMlh-gOI0ukScfJ9BA',
+            title: 'Free Britney, Bitch!',
+            txt: ''
+        },
+        style: {
+            backgroundColor: 'white',
+            color: 'black'
+        }
     }
-},
-{
-    id: 'TempTry4',
-    type: 'noteVideo',
-    isPinned: false,
-    info: {
-        url: 'https://i.ytimg.com/an_webp/CduA0TULnow/mqdefault_6s.webp?du=3000&sqp=COzt44EG&rs=AOn4CLBxY6Ssg3C6cMlh-gOI0ukScfJ9BA',
-        title: 'Free Britney, Bitch!',
-        txt: ''
-    },
-    style: {
-        backgroundColor: 'white',
-        color: 'black'
-    }
-}
 ];
 
 const keeps = query();
@@ -70,22 +70,8 @@ export const keepService = {
     removeKeep,
     newKeep,
     saveKeep,
-    getKeepById,
-    updateKeep
+    getKeepById
 }
-
-function updateKeep(id, whatTodo) {
-    return getKeepById(id)
-        .then((note) => {
-            switch (whatTodo) {
-                case 'togglePin':
-                    note.isPinned = !note.isPinned
-                    saveKeep(note)
-                    return note;
-            }
-        })
-}
-
 
 function query() {
     return asyncStorageService.query(KEEPS_KEY)
@@ -162,6 +148,9 @@ function getKeepById(id) {
 }
 
 function saveKeep(keep) {
-    if (keep.id) return asyncStorageService.put(KEEPS_KEY,keep);
-    else return asyncStorageService.post(KEEPS_KEY,keep);
+    return keeps
+        .then((keeps) => {
+            keeps.push(keep)
+            asyncStorageService.save(KEEPS_KEY, keeps)
+        })
 }
