af8bd4f9e29fa3822568cc8b5fe206270c6dbb5c
Noa Reisman <noareisman10@gmail.com>
2021-02-26 23:13:32 +0200

    temp 18

--- js/apps/keep/cmps/keep-list.cmp.js
+++ js/apps/keep/cmps/keep-list.cmp.js
@@ -3,11 +3,18 @@ import notePrev from './note-prev.cmp.js';
 export default {
     props: ['notes'],
     template: `
-         <ul class="keep-notes-container flex space-between"> 
-             <li class="note-item" v-for="note in notes" :key="note.id">
-                 <note-prev :note="note"/>
-             </li>
-         </ul>`,
+        <section>
+            <ul class="keep-notes-container pinned-notes flex space-between"> 
+                <li class="note-item" v-for="note in notes" :key="note.id">
+                    <note-prev :note="note"/>
+                </li>
+            </ul>
+            <ul class="keep-notes-container flex space-between"> 
+                <li class="note-item" v-for="note in notes" :key="note.id">
+                    <note-prev :note="note"/>
+                </li>
+            </ul>
+        </section>`,
     components: {
         notePrev
     },
