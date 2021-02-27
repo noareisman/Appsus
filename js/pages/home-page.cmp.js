43b3d3b7484b31034ce1ac1e97190d5982f1437c
Noa Reisman <noareisman10@gmail.com>
2021-02-27 22:03:41 +0200

    h

--- js/pages/home-page.cmp.js
+++ js/pages/home-page.cmp.js
@@ -1,6 +1,6 @@
 export default {
 
     template: `
-    <section>Hi i am main home page </section>
+    <img class="homePage" src="https://images.unsplash.com/uploads/141103282695035fa1380/95cdfeef?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=3588&q=80"/>
     `
 }
