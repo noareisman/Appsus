324bd281a3fb46b07dd6e7340e1c97e8c50567c1
ReamLasry <ream794613@gmail.com>
2021-02-27 21:49:37 +0200

    update

--- js/apps/keep/cmps/note-video.cmp.js
+++ js/apps/keep/cmps/note-video.cmp.js
@@ -4,7 +4,7 @@ export default {
     <section class="note-type">
         <h1>{{info.title}}</h1>
         <div>
-            <iframe width="400" height="250" :src="info.url" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="340" height="250" :src="info.url" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
         <h2>{{info.txt}}</h2>
     </section> `
