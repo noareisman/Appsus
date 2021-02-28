2f09fa1c6e85ef3120b3ba09f20c2a445e91edd3
ReamLasry <ream794613@gmail.com>
2021-02-28 09:00:26 +0200

    update

--- js/apps/keep/cmps/new-note.cmp.js
+++ js/apps/keep/cmps/new-note.cmp.js
@@ -9,7 +9,7 @@ export default {
                 <textarea class="keep-text-area" v-model="textDesc" placeholder="Enter text here..." name="Text1" cols="100"></textarea>
                 <div class="new-keep-btns flex column">
                     <button class="pin-keep-btn" :class="isPinned" @click="pinKeep"><img src="./images/keepType/pinkeep.png" alt="" /></button>
-                    <button class="save-keep-btn" @click="saveNote"><img src="./images/sendkeep.png" alt="" /></button>
+                    <button class="save-keep-btn" @click="saveNote"><img src="./images/keepType/sendkeep.png" alt="" /></button>
                 </div>
             </div>
             <div class="colors-choise flex space-around">
